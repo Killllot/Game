58f03cd6074ef846b7dee93161d45cc191a6309a
Кирилл Филатов <fillontino@gmail.com>
2020-11-21 17:43:37 +0300

    Добавьте файлы проекта.

---
+++ OurGame/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace OurGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.time_cloud = new System.Windows.Forms.Timer(this.components);
+            this.mainPlayer = new System.Windows.Forms.PictureBox();
+            this.LeftMove = new System.Windows.Forms.Timer(this.components);
+            this.RightMove = new System.Windows.Forms.Timer(this.components);
+            this.UpMove = new System.Windows.Forms.Timer(this.components);
+            this.DownMove = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // time_cloud
+            // 
+            this.time_cloud.Enabled = true;
+            this.time_cloud.Interval = 30;
+            this.time_cloud.Tick += new System.EventHandler(this.time_cloud_Tick);
+            // 
+            // mainPlayer
+            // 
+            this.mainPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.mainPlayer.Image = global::OurGame.Properties.Resources.cowboy;
+            this.mainPlayer.Location = new System.Drawing.Point(373, 353);
+            this.mainPlayer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mainPlayer.Name = "mainPlayer";
+            this.mainPlayer.Size = new System.Drawing.Size(94, 98);
+            this.mainPlayer.TabIndex = 0;
+            this.mainPlayer.TabStop = false;
+            // 
+            // LeftMove
+            // 
+            this.LeftMove.Interval = 10;
+            this.LeftMove.Tick += new System.EventHandler(this.LeftMove_Tick);
+            // 
+            // RightMove
+            // 
+            this.RightMove.Interval = 10;
+            this.RightMove.Tick += new System.EventHandler(this.RightMove_Tick);
+            // 
+            // UpMove
+            // 
+            this.UpMove.Interval = 10;
+            this.UpMove.Tick += new System.EventHandler(this.UpMove_Tick);
+            // 
+            // DownMove
+            // 
+            this.DownMove.Interval = 10;
+            this.DownMove.Tick += new System.EventHandler(this.DownMove_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(948, 553);
+            this.Controls.Add(this.mainPlayer);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(964, 592);
+            this.MinimumSize = new System.Drawing.Size(964, 592);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer time_cloud;
+        private System.Windows.Forms.PictureBox mainPlayer;
+        private System.Windows.Forms.Timer LeftMove;
+        private System.Windows.Forms.Timer RightMove;
+        private System.Windows.Forms.Timer UpMove;
+        private System.Windows.Forms.Timer DownMove;
+    }
+}
+
---
+++ OurGame/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OurGame
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] cloud;
+        int cloudspeed;
+        int PlayerSpeed = 5;
+        Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            cloudspeed = 5;
+            cloud = new PictureBox[20];
+            rnd = new Random();
+            for (int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i] = new PictureBox();
+                cloud[i].BorderStyle = BorderStyle.None;
+                cloud[i].Location = new Point(rnd.Next(-1000, 1280), rnd.Next(140, 320));
+                if (i % 2 == 1)
+                {
+                    cloud[i].Size = new Size(rnd.Next(100, 125), rnd.Next(30, 70));
+                    cloud[i].BackColor = Color.FromArgb(rnd.Next(125, 125), 225, 200, 200);
+                }
+                else
+                {
+                    cloud[i].Size = new Size(150, 25);
+                    cloud[i].BackColor = Color.FromArgb(rnd.Next(90, 125), 225, 205, 205);
+                }
+                this.Controls.Add(cloud[i]);
+            }
+        }
+
+        private void time_cloud_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cloud.Length; i++)
+            {
+                cloud[i].Left += cloudspeed - 4;
+                if (cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = cloud[i].Height;
+                }
+            }
+            for (int i = cloud.Length; i < cloud.Length; i++)
+            {
+                cloud[i].Left += cloudspeed - 10;
+                if (cloud[i].Left >= 1280)
+                {
+                    cloud[i].Left = cloud[i].Left;
+                }
+            }
+        }
+            private void LeftMove_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Left>10)
+            {
+                mainPlayer.Left -= PlayerSpeed;
+            }
+        }
+
+        private void RightMove_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Left<850)
+            {
+                mainPlayer.Left += PlayerSpeed;
+            }
+        }
+        private void UpMove_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Top>320)
+            {
+                mainPlayer.Top -= PlayerSpeed;
+            }
+            
+        }
+
+        private void DownMove_Tick(object sender, EventArgs e)
+        {
+            if (mainPlayer.Top<450)
+            {
+                mainPlayer.Top += PlayerSpeed;
+            } 
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            mainPlayer.Image = Properties.Resources.cowboy_run;
+            if(e.KeyCode == Keys.Up)
+            {
+                UpMove.Start();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                DownMove.Start();
+            }
+            if (e.KeyCode==Keys.Left)
+            {
+                
+                LeftMove.Start();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                
+                RightMove.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            mainPlayer.Image = Properties.Resources.cowboy;
+
+            LeftMove.Stop();
+            RightMove.Stop();
+            UpMove.Stop();
+            DownMove.Stop();
+
+        }
+
+        
+    }
+    }
+
