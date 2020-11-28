ab411ccca0290cf3f63e9c32e07a187cdc173851
Кирилл Филатов <fillontino@gmail.com>
2020-11-28 15:03:57 +0300

    начал разбираться с дисплеем

--- OurGame/Properties/Resources.Designer.cs
+++ OurGame/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace OurGame.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap background_01 {
+            get {
+                object obj = ResourceManager.GetObject("background_01", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cowboy {
             get {
                 object obj = ResourceManager.GetObject("cowboy", resourceCulture);
