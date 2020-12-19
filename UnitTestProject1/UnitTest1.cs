65f5cbdccb10b901f2f3c25217b8c7e5fb787ecc
Кирилл Филатов <fillontino@gmail.com>
2020-12-19 15:33:36 +0300

    Test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.IO;
+using System.Text.RegularExpressions;
+using OurGame;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace UnitTestProject1
 {
@@ -9,7 +13,14 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            
+            string WrightList = @"myList.txt";
+            string x;
+            using (StreamReader rd = new StreamReader(WrightList))
+            {
+                x = "" + rd;
+            }
+            StringAssert.Matches(x, new Regex(@"\d{0}"));
+
 
         }
     }
